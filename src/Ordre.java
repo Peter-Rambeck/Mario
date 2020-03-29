f22e5b02bae10547386f924bdbb2993c344bade5
Peter-Rambeck <peter@indochinaexplorers.com>
2020-03-29 14:47:38 +0200

    Update 29.03. 14:20

---
+++ src/Ordre.java
@@ -0,0 +1,44 @@
+import java.sql.Time;
+import java.util.ArrayList;
+
+public class Ordre {
+
+    static int ordreCounter = 1;
+    ArrayList<Pizza> pizzaer;
+    Time afhentningsTidspunkt;
+    int ordreId;
+
+    // New variable
+    int pris;
+
+    public Ordre (ArrayList<Pizza> pizzaer, Time afhentningsTidspunkt) {
+        this.afhentningsTidspunkt = afhentningsTidspunkt;
+        this.ordreId = ordreCounter;
+        ordreCounter++;
+
+//        for (Pizza arrayList : this.pizzaer = pizzaer) {
+
+        }
+
+    /*
+ visOrdrePris skal udregne ordre prisen
+*/
+    public void setPris() {
+        int tmpPris = 0;
+        // TODO: Calculate orderprice
+        // tmpPris = pizza.getPris();
+        // tmpPris =
+    }
+
+    public int visOrdrePris() {
+        int pris = 0;
+        return pris;
+    }
+
+    /*
+  visOrdre skal udskrive den fulde ordre: getPizzaer + afhentningsTidspunkt + pris
+   */
+    public void visOrdre() {
+    }
+
+}
