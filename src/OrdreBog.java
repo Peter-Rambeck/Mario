b1a6f8c6ffed084d8a990e21fb3cbd32eac5a7a0
jensgelbek <jensgelbek@gmail.com>
2020-04-01 14:48:29 +0200

    f

--- src/OrdreBog.java
+++ src/OrdreBog.java
@@ -1,26 +1,24 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Array;
 import java.util.ArrayList;
 
 import static java.lang.System.lineSeparator;
-import static java.lang.System.setOut;
+
 
 public class OrdreBog {
-
+    //Ordrebogen indeholder en liste af ordrer og dagens omsætning
     ArrayList<Ordre> ordreListe;
     int dagensOmsætning = 0;
 
+    // konstruktor af tom ordrebog
     public OrdreBog(){
-
         ordreListe = new ArrayList<Ordre>();
     }
 
     public void afslutOrdre(Ordre ordre) throws IOException {
 
+        // læg ordrens pris til dagens omsætning
         dagensOmsætning = dagensOmsætning + ordre.samletPris;
-
 
         // skriv ordre til fil(ordre);
         FileWriter fw = null;
@@ -36,18 +34,19 @@ public class OrdreBog {
             }
         fw.flush();
         fw.close();
-
         ordreListe.remove(ordre);
-
     }
 
     public Ordre findOrdre(int nr) throws IOException {
-
+        //find ordre i ordrebog vha ordrenr
+        // gennemløb ordrelisten
         for (Ordre ordre : ordreListe) {
             if (ordre.ordreId == nr){
+                //hvis ordrnr fundet returner ordren
                 return ordre;
             }
         }
+        //returner null hvis ordrnr ikke findes i rdrebog
         return null;
     }
 
