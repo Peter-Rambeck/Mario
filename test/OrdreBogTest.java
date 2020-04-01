30f5d9d5a3e7899dce83abc10b6c2da217e908d8
jensgelbek <jensgelbek@gmail.com>
2020-04-01 14:31:22 +0200

    f

--- test/OrdreBogTest.java
+++ test/OrdreBogTest.java
@@ -1,35 +1,48 @@
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OrdreBogTest {
+    Pizza pizza1;
+    Pizza pizza2;
     Ordre testordre1 = new Ordre();
     Ordre testordre2 = new Ordre();
-    Pizza pizza1=new Pizza(1,"test","ingredienser",45);
-    Pizza pizza2=new Pizza(2,"test2","ingredienser",55);
-    OrdreBog denneOrdreBog =new OrdreBog();
-
-    @Before
-    public void before() throws Exception {
+    OrdreBog denneOrdreBog;
+    @BeforeEach
+    void setUp() {
+        pizza1=new Pizza(1,"test","ingredienser",45);
+        pizza2=new Pizza(2,"test2","ingredienser",55);
+        denneOrdreBog=new OrdreBog();
         testordre1.tilføjPizza(pizza1);
+        testordre1.tilføjKlokkeSlet(LocalTime.parse("12:00"));
         testordre1.tilFøjOrdre(denneOrdreBog);
-        System.out.println("setup");
+
         testordre2.tilføjPizza(pizza2);
+        testordre2.tilføjKlokkeSlet(LocalTime.parse("11:00"));
         testordre2.tilFøjOrdre(denneOrdreBog);
+
     }
+
+
 
 
     @Test
     void findOrdre() throws IOException {
+        //tjekker at ordrebogen: den første ordre der blev lagt ind har nummer et og rette pris
         Ordre fundet;
-        System.out.println(denneOrdreBog);
         fundet=denneOrdreBog.findOrdre(1);
-        System.out.println(fundet);
-        assertEquals(55,fundet.samletPris);
+        assertEquals(45,fundet.samletPris);
 
-        //fail();
+        //tjekker at ordrebogen er sorteret, odren kl 11 er den første i ordrebogen selvom den blev lagt ind sidst
+        fundet=denneOrdreBog.findOrdre(2);
+        assertEquals(fundet, denneOrdreBog.ordreListe.get(0));
+
+
+
     }
 }
