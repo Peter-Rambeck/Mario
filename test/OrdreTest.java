5b51830bb98d4176be078e03970004ec69cb28a3
jensgelbek <jensgelbek@gmail.com>
2020-04-01 12:24:23 +0200

    f

--- test/OrdreTest.java
+++ test/OrdreTest.java
@@ -2,10 +2,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OrdreTest {
+abstract class OrdreTest {
+
+
+
+    public Ordre testordre = new Ordre();
+    public Pizza pizza1=new Pizza(1,"test","ingredienser",45);
+    public Pizza pizza2=new Pizza(2,"test2","ingredienser",55);
+
 
     @Test
     void tilføjPizza() {
+        testordre.tilføjPizza(pizza1);
+        assertEquals(45,testordre.samletPris);
+        testordre.tilføjPizza(pizza2);
+        assertEquals(100,testordre.samletPris);
+
         //fail();
     }
 
