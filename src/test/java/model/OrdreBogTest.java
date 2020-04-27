24624929bc943f4cf390477a2e812daa68b0bd61
jensgelbek <jensgelbek@gmail.com>
2020-04-27 10:21:45 +0200

    f

--- src/test/java/model/OrdreBogTest.java
+++ src/test/java/model/OrdreBogTest.java
@@ -1,12 +1,13 @@
 package model;
 
-import model.Pizza;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
-import model.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrdreBogTest {
@@ -17,16 +18,18 @@ public class OrdreBogTest {
     OrdreBog denneOrdreBog;
     @BeforeEach
     void setUp() {
-    /*    pizza1=new Pizza(1,"test","ingredienser",45);
+        pizza1=new Pizza(1,"test","ingredienser",45);
         pizza2=new Pizza(2,"test2","ingredienser",55);
         denneOrdreBog=new model.OrdreBog();
         testordre1.tilføjPizza(pizza1);
         testordre1.tilføjKlokkeSlet(LocalTime.parse("12:00"));
+        testordre1.setOrdreId(1);
         testordre1.tilFøjOrdre(denneOrdreBog);
 
         testordre2.tilføjPizza(pizza2);
         testordre2.tilføjKlokkeSlet(LocalTime.parse("11:00"));
-        testordre2.tilFøjOrdre(denneOrdreBog);*/
+        testordre1.setOrdreId(2);
+        testordre2.tilFøjOrdre(denneOrdreBog);
 
     }
 
@@ -35,15 +38,16 @@ public class OrdreBogTest {
 
     @Test
     void findOrdre() throws IOException {
-     /*   //tjekker at ordrebogen: den første ordre der blev lagt ind har nummer et og rette pris
-        model.Ordre fundet;
-        fundet=denneOrdreBog.findOrdre(1);
+
+        //tjekker at ordrebogen: den første ordre der blev lagt ind har nummer et og rette pris
+        Ordre fundet=denneOrdreBog.findOrdre(testordre1.ordreId);
         assertEquals(45,fundet.samletPris);
 
-        //tjekker at ordrebogen er sorteret, odren kl 11 er den første i ordrebogen selvom den blev lagt ind sidst
-        fundet=denneOrdreBog.findOrdre(2);
-        assertEquals(fundet, denneOrdreBog.ordreListe.get(0));
-*/
+
+        //tjekker at ordrebogen er sorteret, testordre2 ligger på plads 0
+
+        assertEquals(denneOrdreBog.ordreListe.get(0).ordreId,testordre2.ordreId);
+
 
 
     }
